5b3540c64b3352f2f8e9a8058965ed23ec48811b
frumsrekovni <joshua.backstrom@gmail.com>
2022-07-08 18:45:57 +0200

    - at mobile resolutions. the sidebear dissappears and the posts cover the entire width. - added another post about mnemonics. - Made the picture on post2 be clickable in order to display a larger image. done with typescript.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const modal = document.getElementById("post2-darken-screen");
+function showImage() {
+    modal.style.display = "block";
+}
+function closeModal() {
+    modal.style.display = "none";
+}
---
+++ js/script.ts
@@ -0,0 +1,8 @@
+const modal = document.getElementById("post2-darken-screen") as HTMLElement;
+
+function showImage(){
+    modal.style.display = "block";
+}
+function closeModal(){
+    modal.style.display = "none";
+}
