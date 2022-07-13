d137b9b7fd2933fb84087121a4f51a0e20008f06
frumsrekovni <joshua.backstrom@gmail.com>
2022-07-13 22:50:45 +0200

    - Almost finished a kana flash card post - Polished some of the other posts

--- js/script.js
+++ js/script.js
@@ -1,7 +1,48 @@
 const darkeningElement = document.getElementById("page-darkener");
+const flashSquareKanaElement = document.getElementById("flash-square-kana");
+const flashSquareAnswerElement = document.getElementById("flash-square-answer");
 function darkenPage() {
     darkeningElement.style.display = "block";
 }
 function undarkenPage() {
     darkeningElement.style.display = "none";
+}
+const allHiragana = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+const allKatakana = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン";
+const allHiraganaDiacritics = "がぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ";
+const allKatakanaDiacritics = "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ";
+const allHiraganaDigraphs = "";
+const allKatakanaDigraphs = "";
+const allKanaTuples = [
+    ["あ", "ア", "a"],
+    ["い", "イ", "i"],
+    ["え", "エ", "e"],
+    ["う", "ウ", "u"],
+    ["お", "オ", "o"],
+    ["か", "カ", "ka"],
+    ["き", "キ", "ki"],
+    ["け", "ケ", "ke"],
+    ["く", "ク", "ku"],
+    ["こ", "コ", "ko"],
+    ["さ", "サ", "sa"],
+    ["し", "シ", "shi"],
+    ["す", "ス", "su"],
+    ["せ", "セ", "se"],
+    ["そ", "ソ", "so"],
+    ["た", "タ", "ta"],
+    ["ち", "チ", "chi"],
+    ["つ", "ツ", "tsu"],
+    ["て", "テ", "te"],
+    ["と", "ト", "to"]
+];
+function flashSquareKanaClick() {
+    flashSquareKanaElement.style.display = "none";
+    flashSquareAnswerElement.style.display = "block";
+}
+function flashSquareAnswerClick() {
+    const randomKanaElementPosition = Math.floor(Math.random() * (allKanaTuples.length)); // Any random element from allkana array
+    flashSquareKanaElement.innerText = allKanaTuples[randomKanaElementPosition][Math.floor(Math.random() * 2)]; // Between 0 - 1. Hiragana or katakana is a coinflip
+    flashSquareAnswerElement.innerText = allKanaTuples[randomKanaElementPosition][2]; // The answer
+    flashSquareAnswerElement.style.display = "none";
+    flashSquareKanaElement.style.display = "block";
 }
--- js/script.ts
+++ js/script.ts
@@ -1,8 +1,54 @@
 const darkeningElement = document.getElementById("page-darkener") as HTMLElement;
-
+const flashSquareKanaElement = document.getElementById("flash-square-kana") as HTMLElement;
+const flashSquareAnswerElement = document.getElementById("flash-square-answer") as HTMLElement;
 function darkenPage(){
     darkeningElement.style.display = "block";
 }
 function undarkenPage(){
     darkeningElement.style.display = "none";
+}
+const allHiragana:string = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん"
+const allKatakana:string = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン"
+const allHiraganaDiacritics:string = "がぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ";
+const allKatakanaDiacritics:string = "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ";
+const allHiraganaDigraphs:string ="";
+const allKatakanaDigraphs:string ="";
+
+type kanaTuple = [string,string,string];
+
+const allKanaTuples:kanaTuple[] = [
+    ["あ","ア","a"],
+    ["い","イ","i"],
+    ["え","エ","e"],
+    ["う","ウ","u"],
+    ["お","オ","o"],
+    ["か","カ","ka"],
+    ["き","キ","ki"],
+    ["け","ケ","ke"],
+    ["く","ク", "ku"],
+    ["こ","コ", "ko"],
+    ["さ","サ", "sa"],
+    ["し","シ", "shi"],
+    ["す","ス", "su"],
+    ["せ","セ", "se"],
+    ["そ","ソ", "so"],
+    ["た","タ", "ta"],
+    ["ち","チ", "chi"],
+    ["つ","ツ", "tsu"],
+    ["て","テ", "te"],
+    ["と","ト", "to"]
+];
+
+function flashSquareKanaClick(){
+    flashSquareKanaElement.style.display = "none";
+    flashSquareAnswerElement.style.display = "block";
+}
+
+function flashSquareAnswerClick(){
+    const randomKanaElementPosition = Math.floor(Math.random() * (allKanaTuples.length));                       // Any random element from allkana array
+    flashSquareKanaElement.innerText = allKanaTuples[randomKanaElementPosition][Math.floor(Math.random() * 2)]; // Between 0 - 1. Hiragana or katakana is a coinflip
+    flashSquareAnswerElement.innerText = allKanaTuples[randomKanaElementPosition][2];                           // The answer
+
+    flashSquareAnswerElement.style.display = "none";
+    flashSquareKanaElement.style.display = "block";
 }
