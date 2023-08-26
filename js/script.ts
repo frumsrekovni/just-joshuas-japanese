3dee540f1560f95dbc90a606bdd4a19769c511f8
frumsrekovni <joshua.backstrom@gmail.com>
2023-08-26 21:27:46 +0200

    -more N5

--- js/script.ts
+++ js/script.ts
@@ -542,7 +542,9 @@ const commonJapaneseWords:wordCard[] = [
     ["向こう","向こう むこう Over there<br>向こうへ行け！ むこうへいけ！ Go over there!/Go away<br>向こうの方で泳ごう むこうのほうでおよごう Let's swim over there<br>向こうに白い塔が見えます むこうにしろいとうがみえます You can see a white tower over there"],
     ["困る","困る こまる to be troubled<br>今困っているんだ いまこまっているんだ I'm in trouble now<br>僕はしばしばお金に困る ぼくはしばしばおかねにこまる I'm always running into trouble with money."],
     ["冗談","冗談 じょうだん Joke<br>冗談だよ じょうだんだよ It's a joke<br>冗談はやめて じょうだんはやめて Get serious<br>りんさんは私の冗談に笑った りんさんはわたしのじょうだんにわらった Rin laughed at my joke"],
-    ["浴びる","浴びる あびる to take a shower<br>シャワー浴びるの？ シャワーあびるの？ Are you going to take a shower?<br>彼女は、朝食前にシャワーを浴びることにしている かのじょは、ちょうしょくまえにシャワーをあびることにしている She takes a shower before breakfast"]
+    ["浴びる","浴びる あびる to take a shower<br>シャワー浴びるの？ シャワーあびるの？ Are you going to take a shower?<br>彼女は、朝食前にシャワーを浴びることにしている かのじょは、ちょうしょくまえにシャワーをあびることにしている She takes a shower before breakfast"],
+    ["曇り","曇り くもり Cloudy<br>今日は曇りです きょうはくもりです It's cloudy today<br>曇りの日は嫌い くもりのひはきらい I don't like cloudy days"],
+    ["暖かい","暖かい あたたかい Warm/having enough money​<br>暖かい日でした あたたかいひでした It was a warm day<br>懐が暖かい ふところがあたたかい I have enough money<br>この冬は暖かい。 このふゆはあたたかい。 This winter is warm."]
 
  
 ];
